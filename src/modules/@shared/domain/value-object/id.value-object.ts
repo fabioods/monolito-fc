6a2f8c2ffd33bd46dc88d9d77cb3d04cb724911e
Fabio dos santos <fah_ds@live.com>
2023-06-10 20:36:14 -0300

    Create shared module

---
+++ src/modules/@shared/domain/value-object/id.value-object.ts
@@ -0,0 +1,14 @@
+import { ValueObject } from './value-object.interface'
+import { v4 as uuidv4 } from 'uuid'
+
+export class Id implements ValueObject {
+  private readonly _value: string
+
+  constructor (value?: string) {
+    this._value = value || uuidv4()
+  }
+
+  get value (): string {
+    return this._value
+  }
+}
